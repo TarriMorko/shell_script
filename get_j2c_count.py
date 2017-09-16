acc4827a0a83c6c03e00135dd0b17a9946754298
NMCA56O <0937160git@gmail.com>
2017-09-16 19:40:44 +0800

    運作中的版本，已提供給廠商

--- get_j2c_count.py
+++ get_j2c_count.py
@@ -1,12 +1,14 @@
 import re
 
-search_text = "jdbc"
+search_text = "j2c"
 
 PerfMbeanLists =  AdminControl.queryNames('type=Perf,*').split( lineSeparator)
 
 for perfStr in PerfMbeanLists:
     perfObj = AdminControl.makeObjectName( perfStr)
-    servername = perfObj.getKeyPropertyList()['process'] 
+    servername = perfObj.getKeyPropertyList()['process']
+    if servername == 'nodeagent' or servername == 'dmgr':
+        continue    
     srvrStr = AdminControl.queryNames('type=Server,process=%s,*' % servername)
     srvrObj = AdminControl.makeObjectName( srvrStr)
     params = [ srvrObj, java.lang.Boolean ('true')]
@@ -14,25 +16,18 @@ for perfStr in PerfMbeanLists:
     stats = AdminControl.invoke_jmx( perfObj, 'getStatsObject', params, sigs)
     
 def listStatsSubCollections( object ):
-    try:
-        sub = object.subCollections()
-        if not sub:
-            # print object.getName(), ", No subCollections."
-            target_stat = re.compile( r'.*' + search_text + '*' ).match( object.getName() )
-            if target_stat:
-                print object.getName()    # object.listStatisticNames()
-                print object.getStatistic('PoolSize').getCurrent()            
-            return
-    except:
+
+    sub = object.subCollections()
+    if not sub:
+        target_stat = re.compile( r'.*' + search_text + '*' ).match( object.getName() )
+        if target_stat:
+            print object.getName()
+            print object.getStatistic('PoolSize').getCurrent()            
         return
-    try:
-        #print object.getName(), ", has subCollections..."
-        for sub_item in sub:
-            # print sub_item.getName()
-            sub_name = sub_item.getName()
-            listStatsSubCollections( object.getStats(sub_name) )
-    except:
-        print 'Unknown'
-        return
+
+    for sub_item in sub:
+        sub_name = sub_item.getName()
+        listStatsSubCollections( object.getStats(sub_name) )
+
 
 listStatsSubCollections(stats)
