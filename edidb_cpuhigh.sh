e0037d564c0e2732870c874bb02eb6df10afbded
T420 <0937160git@gmail.com>
2017-10-24 03:37:18 +0100

    錯誤的版本，vmstat -t 要加 1

--- edidb_cpuhigh.sh
+++ edidb_cpuhigh.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 #
 #
-while ( true ); do
+
+LOGFILENAME="edidb_cpuhigh.log"
+
+echo "$(date +"%Y-%m-%d %H:%M:%S")" script start >>${LOGFILENAME}
+
+while (true); do
     sleep 1
-    idle=$(vmstat -t | tail -n 1 | tee -a vmstat.log | awk '{print $(NF-4)}')
-    if [ $idle -lt 10 ]; then
-        echo "$(date +"%Y-%m-%d %H:%M:%S")" trace start >> vmstat.log
-        echo ">>>>${idle}<<<<"
-        echo "$(date +"%Y-%m-%d %H:%M:%S")" trace done >> vmstat.log
+    idle=$(vmstat -t | tail -n 1 | tee -a ${LOGFILENAME} | awk '{print $(NF-4)}')
+    if [ $idle -lt 5 ]; then
+        echo "$(date +"%Y-%m-%d %H:%M:%S")" trace start >>${LOGFILENAME}
+
+        ./perfpmr.sh -x trace.sh 5
+
+        echo "$(date +"%Y-%m-%d %H:%M:%S")" trace done >>${LOGFILENAME}
         exit 0
     fi
 done
