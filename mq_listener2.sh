e5b5a0cec00c9ab8b7c54ca96b8e5f5035e2f02e
T420 <0937160git@gmail.com>
2017-11-06 07:07:24 +0000

    修一下DEBUG功能

--- mq_listener2.sh
+++ mq_listener2.sh
@@ -4,7 +4,6 @@
 # qmanager 啟動狀態下才查得到
 ##	Global Variables for sending SMS Alarm
 
-
 ## Variables and functions for logging
 _DEBUG="on"
 LOGFILENAME="mq_listener2.log"
@@ -35,21 +34,30 @@ writelog() {
 QMGRS=$(dspmq | sed 's/).*//g' | sed 's/.*(//g')
 
 for qmgr in $QMGRS; do
-    echo "qmgr: $qmgr"
-    LISTENERS=$(echo "DISPLAY LISTENER(*)"  | runmqsc $qmgr -e | grep -oP '(?<=LISTENER\().*(?=\))' | grep -v SYSTEM )
-    echo $LISTENERS
+    DEBUG "qmgr: $qmgr"
+    LISTENERS=$(echo "DISPLAY LISTENER(*)" |
+        runmqsc $qmgr -e |
+        grep -oP '(?<=LISTENER\().*(?=\))' |
+        grep -v SYSTEM)
+
+    DEBUG LISTENERS: $LISTENERS
 
     for listener in $LISTENERS; do
-        port_of_listener=$(echo "DISPLAY LISTENER(${listener})" | runmqsc $qmgr -e |  grep -oP '(?<=PORT\().*(?=\))')
-        echo "在 Qmanager: $qmgr 裡面的 listener: $listener 開這個 port $port_of_listener "
+        port_of_listener=$(echo "DISPLAY LISTENER(${listener})" |
+            runmqsc $qmgr -e |
+            grep -oP '(?<=PORT\().*(?=\))')
 
-        netstat -tunel  | grep LISTEN | grep $port_of_listener -q
+        DEBUG "在 Qmanager: $qmgr 裡面的 listener: $listener 開這個 port $port_of_listener "
+
+        netstat -tunel | grep LISTEN | grep $port_of_listener -q
+
         if [ $? -eq 0 ]; then
-            echo "有開啦"
+            DEBUG "Listener : $listener is running"
         else
-            writelog "Warning !! Listner $listener in $qmgr on port $port_of_listener are not LISTEN."
-            echo "start LISTENER" | runmqsc $qmgr
+            echo "start listener($listener)" | runmqsc $qmgr
+            writelog "Start Listener : $listener with port $port_of_listener in QMGR $qmgr"
+            sleep 1
         fi
     done
 
-done 
+done
