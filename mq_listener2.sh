1f526badea61338ada26519a28df9731197ff92c
T420 <0937160git@gmail.com>
2017-11-06 06:42:10 +0000

    加入簡訊通知

--- mq_listener2.sh
+++ mq_listener2.sh
@@ -2,6 +2,35 @@
 #
 #
 # qmanager 啟動狀態下才查得到
+##	Global Variables for sending SMS Alarm
+
+
+## Variables and functions for logging
+_DEBUG="on"
+LOGFILENAME="mq_listener2.log"
+
+DEBUG() {
+    [ "$_DEBUG" == "on" ] && echo "$(date +"%Y-%m-%d %H:%M:%S") $@" || :
+}
+
+writelog() {
+    # Creat SMS Alarm
+    ACCOUNT=AA34567
+    FILENAME_PRIFIX=${ACCOUNT}$(date +%Y%s)
+    TXT_FILE="${FILENAME_PRIFIX}.txt"
+    END_FILE="${FILENAME_PRIFIX}.end"
+    TXT_MODE="C"
+    PHONE_NUMBER="0900123456 0900345988"
+    log_message=$@
+    echo "$(date +"%Y-%m-%d %H:%M:%S") ${log_message}" | tee -a ${LOGFILENAME}
+    echo "" >$END_FILE
+    echo $@ >$TXT_FILE
+    echo $TXT_MODE >>$TXT_FILE
+    for phone in $PHONE_NUMBER; do
+        echo $phone >>$TXT_FILE
+    done
+    # exit 0
+}
 
 QMGRS=$(dspmq | sed 's/).*//g' | sed 's/.*(//g')
 
@@ -18,6 +47,7 @@ for qmgr in $QMGRS; do
         if [ $? -eq 0 ]; then
             echo "有開啦"
         else
+            writelog "Warning !! Listner $listener in $qmgr on port $port_of_listener are not LISTEN."
             echo "start LISTENER" | runmqsc $qmgr
         fi
     done
