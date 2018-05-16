78bb2dfbcd7551641189471e86d5248fce75b60d
t420 <0937160git@gmail.com>
2018-05-16 17:48:44 +0800

    AIX 的部分測試完了

--- check_permission_and_md5.sh
+++ check_permission_and_md5.sh
@@ -10,25 +10,27 @@
 ## 此時系統顯示兩個選項 "檢查檔案權限" "檢查檔案hash"
 ## 宗翰選擇了 "檢查檔案權限"
 ## 系統顯示 "請稍候片刻"，並且開始蒐集指定宗翰在設定檔中指定目錄的檔案權限
-## 數分鐘後系統將權限與 baseline 不符合的檔案顯示在螢幕上。並且產生報表到指定目錄。
-## 宗翰按下任意鍵後，系統顯示兩個選項 "檢查檔案權限" "檢查檔案hash"
+## 數分鐘後系統顯示 Audit passed. 或  Audit failed. 並產生報表到指定目錄。
+##
 ## 宗翰做事很有條理
+## 宗翰按下任意鍵後，系統顯示兩個選項 "檢查檔案權限" "檢查檔案hash"
+##
 ## 宗翰選擇了"檢查檔案hash"
 ## 系統顯示 "請稍候片刻"，並且開始蒐集指定宗翰在設定檔中指定目錄的hash
-## 數分鐘後系統將 hash 與 baseline 不符合的檔案顯示在螢幕上。並且產生報表到指定目錄。
+## 數分鐘後系統顯示 Audit passed. 或  Audit failed. 並產生報表到指定目錄。
 ## 宗翰感覺開心，將報表取出後下班了。
 
-_HOME="/src/mwadmin/check_permission_and_md5"
+_HOME="/source/check_permission"
 
 BASE_PERMISSION="$_HOME/BASE_PERMISSION"
 PERMISSION_REPORT="$_HOME/PERMISSION_report_$(hostname)_$(date +%Y%m%d).txt"
 # DIRECTORY_YOU_WAT_TO_CHECK_PERMISSION="/bin /sbin /usr/bin /usr/sbin /etc"
-DIRECTORY_YOU_WAT_TO_CHECK_PERMISSION="/etc"
+DIRECTORY_YOU_WAT_TO_CHECK_PERMISSION="/nmon /etc /bin"
 
 BASE_MD5="$_HOME/BASE_MD5"
 MD5_REPORT="$_HOME/MD5_report_$(hostname)_$(date +%Y%m%d).txt"
 # DIRECTORY_YOU_WAT_TO_CHECK_MD5="/bin /sbin /usr/bin /usr/sbin /etc"
-DIRECTORY_YOU_WAT_TO_CHECK_MD5="/etc"
+DIRECTORY_YOU_WAT_TO_CHECK_MD5="/nmon /etc /bin"
 
 if [[ "$(uname)" = "Linux" ]]; then
   OS="Linux"
@@ -59,16 +61,16 @@ create_base_permission() {
   echo "Please wait..."
   rm $BASE_PERMISSION >/dev/null 2>&1
   for dir in $DIRECTORY_YOU_WAT_TO_CHECK_PERMISSION; do
-    echo "Parsing $dir now..."
+    echo "Parsing $dir permission now..."
     echo ""
     if [[ $OS = "Linux" ]]; then
       time find $dir -type f -exec stat -c '%A %F %g %u %s %Y %n' {} \; >>$BASE_PERMISSION
+      # echo "TODO"
     else
-      time find $dir -type f -exec istat {} \; | tr '\n' ' ' >>$BASE_PERMISSION
 
-      for i in $(find $dir -type f); do
-        istat $i | tr '\n' ' '>>$BASE_PERMISSION
-        echo '\n' >>$BASE_PERMISSION
+      for i in $(find $dir -type f | grep -v -f exclude_file); do
+        echo $i" \c" >>$BASE_PERMISSION
+        istat $i | tr '\n' ' ' | awk '{print $8, $10, $12}' >>$BASE_PERMISSION
       done
 
     fi
@@ -82,17 +84,17 @@ create_permission_today() {
   echo "Please wait..."
   _permission_today="$RANDOM"_temp
   for dir in $DIRECTORY_YOU_WAT_TO_CHECK_PERMISSION; do
-    echo "Parsing $dir now..."
+    echo "Parsing $dir permission now..."
     echo ""
     if [[ $OS = "Linux" ]]; then
       time find $dir -type f -exec stat -c '%A %F %g %u %s %Y %n' {} \; >>$_permission_today
+      # echo "TODO"
     else
 
-      for i in $(find $dir -type f); do
-        istat $i | tr '\n' ' '>>$_permission_today
-        echo '\n' >>$_permission_today
+      for i in $(find $dir -type f | grep -v -f exclude_file); do
+        echo $i" \c" >>$_permission_today
+        istat $i | tr '\n' ' ' | awk '{print $8, $10, $12}' >>$_permission_today
       done
-
 
     fi
 
@@ -100,7 +102,7 @@ create_permission_today() {
 }
 
 diff_permission_today_with_BASE() {
-  diff ${BASE_PERMISSION} $_permission_today -q >/dev/null 2>&1
+  diff ${BASE_PERMISSION} $_permission_today >/dev/null 2>&1
   if [[ $? -eq 0 ]]; then
     echo ''
     echo "Permission Audit passed."
@@ -124,12 +126,17 @@ create_base_md5() {
   echo "Please wait..."
   rm $BASE_MD5 >/dev/null 2>&1
   for dir in $DIRECTORY_YOU_WAT_TO_CHECK_MD5; do
-    echo "Parsing $dir now..."
+    echo "Parsing $dir hash now..."
     echo ""
     if [[ $OS = "Linux" ]]; then
       time find $dir -type f -exec md5sum {} \; >>$BASE_MD5
+      # echo "TODO"
     else
-      time find $dir -type f -exec csum -h MD5 {} \; >>$BASE_MD5
+
+      for i in $(find $dir -type f | grep -v -f exclude_file); do
+        csum -h MD5 $i >>$BASE_MD5
+      done
+
     fi
 
   done
@@ -142,30 +149,35 @@ create_md5_today() {
   _md5_today="$RANDOM"_temp
 
   for dir in $DIRECTORY_YOU_WAT_TO_CHECK_MD5; do
-    echo "Parsing $dir now..."
+    echo "Parsing $dir hash now..."
     echo ""
     if [[ $OS = "Linux" ]]; then
       time find $dir -type f -exec md5sum {} \; >>$_md5_today
+      # echo "TODO"
     else
-      time find $dir -type f -exec csum -h MD5 {} \; >>$_md5_today
+
+      for i in $(find $dir -type f | grep -v -f exclude_file); do
+        csum -h MD5 $i >>$_md5_today
+      done
+
     fi
   done
 
 }
 
 diff_md5_today_with_BASE() {
-  diff ${BASE_MD5} $_md5_today -q >/dev/null 2>&1
+  diff $BASE_MD5 $_md5_today >$MD5_REPORT
 
   if [[ $? -eq 0 ]]; then
-    echo ''
+    echo ""
     echo "MD5 Audit passed."
-    echo "MD5 Audit passed." >$MD5_REPORT
+    echo "MD5 Audit passed." >>$MD5_REPORT
   else
-    diff ${BASE_MD5} $_md5_today >$MD5_REPORT
-    echo '' >>$MD5_REPORT
-    echo "Audit failed. " >>$MD5_REPORT
-    echo "Audit failed, check $MD5_REPORT for detail."
+    echo ""
+    echo "MD5 Audit failed. check $MD5_REPORT for detail."
+    echo "MD5 Audit failed." >>$MD5_REPORT
   fi
+
   rm $_md5_today
 }
 
