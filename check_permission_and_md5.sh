50e169988f8f3a3fb3b483af95b58c077dc86360
T450 <0937160git@gmail.com>
2020-09-15 15:23:22 +0800

    接手修改，當檢查所有可登入使用者對所有目錄皆無權限時、顯示提示訊息。

--- check_permission_and_md5.sh
+++ check_permission_and_md5.sh
@@ -1,102 +1,29 @@
 #!/bin/sh
+# date : 2020-09-15
+# line  #34  #78  #45:${ACCESS_REPORT}
 #
-#
-# 建置帳號對程式及資料檔案相關權限之檢查功能介面，於帳號清查作業時一併列示清查
-# 使用前請先定義 以下參數
-# DIRECTORY_YOU_WANT_TO_CHECK_PERMISSION  #掃描這些目錄下所有檔案與目錄的權限
-# DIRECTORY_YOU_WANT_TO_CHECK_MD5         #掃描這些目錄下所有檔案的hash
-# DIRECTORY_YOU_WANT_TO_CHECK             #檢查誰可以存取這些目錄
+# �ظm�b����{���θ���ɮ׬����v�����ˬd�\�श���A��b���M�d�@�~�ɤ@�֦C�ܲM�d
+# �ϥΫe�Х��w�q �H�U�Ѽ�
+# skip_check    # �p�G�]�w���O "n" �h���L�ˬd
+# DIRECTORY_YOU_WANT_TO_CHECK_PERMISSION  #���y�o�ǥؿ��U�Ҧ��ɮ׻P�ؿ����v��
 
-## USER STORY
+rm -f ${0%/*}/*.temp
 
-## 宗翰正準備執行帳號清查作業，宗翰登入系統打開了 opmenu
-## 宗翰選擇了 check_permission_and_md5 項目
-## 此時系統顯示兩個選項 "檢查檔案權限" "檢查檔案hash"
-## 宗翰選擇了 "檢查檔案權限"
-## 系統顯示 "請稍候片刻"，並且開始蒐集指定宗翰在設定檔中指定目錄的檔案權限
-## 數分鐘後系統顯示 Audit passed. 或  Audit failed. 並產生報表到指定目錄。
-##
-## 宗翰做事很有條理
-## 宗翰按下任意鍵後，系統顯示兩個選項 "檢查檔案權限" "檢查檔案hash"
-##
-## 宗翰選擇了"檢查檔案hash"
-## 系統顯示 "請稍候片刻"，並且開始蒐集指定宗翰在設定檔中指定目錄的hash
-## 數分鐘後系統顯示 Audit passed. 或  Audit failed. 並產生報表到指定目錄。
-## 宗翰感覺開心，將報表取出後下班了。
-
-# Changelog
-
-## [Unreleased]
-# AIX 有 suid 的部分，在比對檔案權限時有問題(多一個 suid欄位)
-# 產生檔案與目錄權限基準檔 但是要by帳號
-
-##
-## [0.8] - 2018-08-10
-### fixed
-# - 保留 1, 2, 3, 4, 11 項功能
-# - 11. 改為 5. 其餘刪除
-
-##
-## [0.7.5] - 2018-07-27
-### Added
-# - 12. 列出某帳號可以讀取、寫入、執行、哪些目錄以及其子目錄
-
-##
-## [0.7] - 2018-07-27
-### Added
-# - 11. 列出某帳號可以讀取、寫入、執行、哪些目錄
-
-## [0.6] - 2018-07-26
-### Added
-# - 8. 列出某帳號可以讀取哪些目錄
-# - 9. 列出某帳號可以寫入哪些目錄
-# - 10. 列出某帳號可以執行哪些目錄
-
-## [0.5] - 2018-07-25
-### Added
-# - 指定一或多個目錄，列出哪些帳號具有該目錄的「讀取」權限
-# - 指定一或多個目錄，列出哪些帳號具有該目錄的「寫入」權限
-# - 指定一或多個目錄，列出哪些帳號具有該目錄的「執行」權限
-### Fixed
-# - 檢查檔案權限 改為 檢查檔案及目錄權限
-# - 產生檔案權限基準檔 改為 產生檔案及目錄權限基準檔
-# - 移除查「某個帳號」在「某個目錄」下、同時「讀取、寫入、執行」權限的所有目錄
-# - 移除查「某個帳號」在「某個目錄」下、有讀取/寫入/執行權限的所有目錄
-
-## [0.4] - 2018-06-05
-### Added
-# - 增加功能8, 查「某個帳號」在「某個目錄」下、同時「讀取、寫入、執行」權限的所有目錄
-# - 增加三個 func, 查「某個帳號」在「某個目錄」下、有讀取/寫入/執行權限的所有目錄
-
-## [0.3] - 2018-05-16
-### Fixed
-# - 修正 linux 的部分，已在 SuSE12sp1, Centos7.4 測試過.
-# - AIX 的部分測試完了.
-
-## [0.2] - 2018-05-11
-### Fixed
-# - 修正 check_permission_and_md5 的AIX istat換行.
-
-## [0.1] - 2018-05-07
-### Added
-# - First audit release.
-# - 1. 檢查檔案權限
-# - 2. 檢查檔案 hash
-# - 3. 產生檔案權限基準檔      
-# - 4. 產生檔案 hash 基準檔
-# - User story.
-
-_HOME="/src/mwadmin/check_permission_and_md5"
-
-BASE_PERMISSION="$_HOME/BASE_PERMISSION"
-PERMISSION_REPORT="$_HOME/PERMISSION_report_$(hostname)_$(date +%Y%m%d).txt"
-DIRECTORY_YOU_WANT_TO_CHECK_PERMISSION="/bin /sbin /usr/bin /usr/sbin /etc"
-
-BASE_MD5="$_HOME/BASE_MD5"
-MD5_REPORT="$_HOME/MD5_report_$(hostname)_$(date +%Y%m%d).txt"
-DIRECTORY_YOU_WANT_TO_CHECK_MD5=$DIRECTORY_YOU_WANT_TO_CHECK_PERMISSION
-
-DIRECTORY_YOU_WANT_TO_CHECK="/home /home/spos2 /src"
+skip_check="n"
+DIRECTORY_YOU_WANT_TO_CHECK="/tmp /tmp/bin /none1 /source"
+_DIR=$DIRECTORY_YOU_WANT_TO_CHECK
+direct_option="direct"
+option_1="$1"
+_HOME="/src/chkau/report"
+[ -d "${_HOME}" ] || mkdir -p ${_HOME}
+dc01_home="/home/dc01"
+[ -d "${dc01_home}" ] || mkdir -p ${dc01_home}
+dc01_REPORT="${dc01_home}/ACCESS_report_$(hostname)_$(date +%Y%m%d).txt"
+ACCESS_REPORT="${_HOME}/ACCESS_report_$(hostname)_$(date +%Y%m%d).txt"
+typeset -i x
+typeset -i c1
+typeset -i c2
+typeset -i wc_
 
 if [[ "$(uname)" = "Linux" ]]; then
   OS="Linux"
@@ -104,21 +31,19 @@ else
   OS="AIX"
 fi
 
+#OS="AIX"
+
 show_main_menu() {
   # Just show main menu.
   clear
   cat <<EOF
   +====================================================================+
+       �D�t�αb����~�ȵ{���θ���ɮפ������v���d��
        Hostname: $(hostname), Today is $(date +%Y-%m-%d)
   +====================================================================+
 
-      1. 檢查檔案與目錄權限
-      2. 檢查檔案 hash
-
-      3. 產生檔案與目錄權限基準檔
-      4. 產生檔案 hash 基準檔
-      
-      5. 列出某帳號可以讀取、寫入、執行、哪些目錄
+      1. ����b���v���ˬd�C�N���G�g�J ${dc01_REPORT}
+      2. �C�X���浲�G
 
       q.QUIT
 
@@ -126,211 +51,230 @@ EOF
 }
 
 list_dirs_permissions_by_user() {
-  # 取出 /etc/passwd 中，sh 為 /bin/bash 或 /bin/ksh 的帳號
-  # 檢查這些帳號是否可以 read write exec 變數 DIRECTORY_YOU_WANT_TO_CHECK 中指定的目錄
-  # 以帳號別列出
-  #
-  # 範例
-
+  # example
+  # 
   # spos2    read       exec /home
 
-  ids=$(cat /etc/passwd | awk -F':' '/.*sh$/  {print $1}')
+  False=1
+  True=0
+  ONE_OF_DIRECTORY_HAS_RWX_PERMISSION=$False
 
-  for id in $ids; do
+  cat /dev/null > ${ACCESS_REPORT}
+  check_t=""
 
-    for _dir in $DIRECTORY_YOU_WANT_TO_CHECK; do
+  if [ "${skip_check}" != "n" ] ; then
+    skip_check_y
+    if [ "${option_1}" != "${direct_option}" ] ; then
+      cp_report
+    fi
+    return
+  fi
 
+  if [[ "$OS" = "Linux" ]]; then
+    ids=$(grep "^AllowUsers" /etc/ssh/sshd_config | tr ' ' '\n' | grep -v '@' | grep -v AllowUsers )
+  else
+
+    ex_ids=$(grep "^AllowUsers" /etc/ssh/sshd_config | tr ' ' '\n' | grep '@' | awk -F@ '{print $1}' | sort | uniq)
+
+    tmp_ids1=${0%/*}/tmp_ids1.${RANDOM}.temp
+    tmp_ids2=${0%/*}/tmp_ids2.${RANDOM}.temp
+    lsuser ALL 2> /dev/null | grep rlogin=true | awk '{print $1}' > ${tmp_ids1}
+    #cat /src/chkau/reference/lsuser_ALL_permission | grep rlogin=true | awk '{print $1}' > ${tmp_ids1}
+
+    for i in ${ex_ids} ; do
+      grep -v ${i} ${tmp_ids1} > ${tmp_ids2}
+      cat ${tmp_ids2} > ${tmp_ids1}
+    done
+    ids=$(cat ${tmp_ids1})
+    rm -f ${tmp_ids1} ${tmp_ids2}
+
+  fi
+
+  if [[ -z "${ids}" ]]; then
+    ids=$(cat /etc/passwd | awk -F':' '/.*sh$/  {print $1}')
+  fi
+
+  if [ "${option_1}" != "${direct_option}" ] ; then
+    echo "Please wait..."
+  fi
+
+  echo "                                 �ϥΪ̹ﭫ�n�t�Φ��A�����n�~���ɮ׻P�{���v���M�d" >>${ACCESS_REPORT}
+  echo '' >>${ACCESS_REPORT}
+  echo HOSTNAME: $(hostname) "    " TIME: $(date +%Y/%m/%d) $(date +%H:%M:%S) >>${ACCESS_REPORT}
+  echo "" >>${ACCESS_REPORT}
+
+  check_1=0
+  for i in ${DIRECTORY_YOU_WANT_TO_CHECK} ; do
+    if [ -d "${i}" ] ; then
+       check_t="${check_t} ${i}"
+    else
+       echo "${i} ���s�b�I  �нT�{ DIRECTORY_YOU_WANT_TOCHECK �]�w��" >>${ACCESS_REPORT}
+    fi
+  done
+
+  DIRECTORY_YOU_WANT_TO_CHECK="${check_t}"
+
+  check_box="�f��Τf����ΡA�N�}��R��"
+  step_n=28
+
+  for id in ${ids} ; do
+    table_head="0"
+    for _dir in ${DIRECTORY_YOU_WANT_TO_CHECK} ; do
       _readable=""
       _writable=""
       _execable=""
+      su ${id} -c "test -r '${_dir}'" >/dev/null 2>&1 && _readable="read"
+      su ${id} -c "test -w '${_dir}'" >/dev/null 2>&1 && _writable="write"
+      su ${id} -c "test -x '${_dir}'" >/dev/null 2>&1 && _execable="exec"
 
-      su - $id -c "test -r '$_dir'" >/dev/null 2>&1 && _readable="read"
-      su - $id -c "test -w '$_dir'" >/dev/null 2>&1 && _writable="write"
-      su - $id -c "test -x '$_dir'" >/dev/null 2>&1 && _execable="exec"
+      if ! [[ "${_readable}" = "" && "${_writable}" = "" && "${_execable}" = "" ]]; then
+        ONE_OF_DIRECTORY_HAS_RWX_PERMISSION=$True
 
-      printf "%-8s %-4s %-5s %-4s %-s \n" $id "$_readable" "$_writable" "$_execable" "$_dir"
+        if [ ${table_head} == "0" ] ; then
+          echo '' >>${ACCESS_REPORT}
+          echo "  �b��     Ū��    �g�J  ����        �ɮשε{�����|                  �t�d��O    �����Hñ��   ���/����ΡA�N�}��R��" >>$ACCESS_REPORT
+          table_head="1"
+        fi
+	    echo '======================================================================================================================' >> ${ACCESS_REPORT}
+
+        if [ ${id} == "cbrusr" ] || [ $(echo ${id} | cut -c1-2) == "sp" ] ||   [ $(echo ${id} | cut -c1-2) == "op" ] ; then
+          Branch="  �t�κ޲z��"
+        elif [ $(echo ${id} | cut -c1-2) == "dc" ] ; then
+          Branch="  ��ƺި��"
+        else
+          Branch=""
+        fi
+
+        wc_t=$(echo ${_dir} | wc -m | awk '{print $1}')
+        wc_=$((${wc_t}-1))
+        x=0
+        c1=1
+        while [ "${wc_}" -gt "${x}" ] ; do
+
+          c2=$((${c1}+${step_n}))
+          if [ ${c2} -gt ${wc_}  ] ; then
+            c2=${wc_}
+          fi
+          if [ ${c1} -gt ${c2} ] ; then
+            break
+          fi
+
+          sub_chr=$(echo ${_dir} | cut -c${c1}-${c2})
+          c1=$((${c2}+1))
+
+          if [ "${x}" -eq 0 ] ; then
+            printf "%-10s %-7s %-5s %-10s %-29s %-12s %-12s %-s \n" "${id}" "${_readable}" "${_writable}" "${_execable}" "${sub_chr}" "${Branch}" "" "${check_box}" >>${ACCESS_REPORT}
+          else
+            printf "%-35s %-s \n" " " "${sub_chr}" >>${ACCESS_REPORT}
+          fi
+          x=$((${x}+${step_n}))
+        done
+        echo '' >>${ACCESS_REPORT}
+
+      #else
+
+        #x=0
+        #wc_=${#_dir}
+        #while [ "${wc_}" -gt "${x}" ] ; do
+        #  if [[ "$OS" = "Linux" ]]; then
+        #    sub_chr=${_dir:${x}:${step_n}}
+        #  else
+        #    sub_chr=$(echo ${_dir} | cut -c${x}-${step_n})
+        #  fi
+        #  if [ "${x}" -eq 0 ] ; then
+        #    printf "%-10s %-27s %-29s \n" ${id} "�L read write exec �v��" "${sub_chr}" >>${ACCESS_REPORT}
+        #  else
+        #    printf "%-35s %-s \n" " " "${sub_chr}" >>${ACCESS_REPORT}
+        #  fi
+        #  x=${x}+${step_n}
+        #done
+
+      fi
+
+      #sub_dirs=$(ls -la ${_dir} | grep "^d" | awk '{print $NF}' | grep -v "^\.")
+      #for sub_dir in $sub_dirs; do
+      #  _readable=""
+      #  _writable=""
+      #  _execable=""
+      #  su ${id} -c "test -r '${_dir}/$sub_dir'" >/dev/null 2>&1 && _readable="read"
+      #  su ${id} -c "test -w '${_dir}/$sub_dir'" >/dev/null 2>&1 && _writable="write"
+      #  su ${id} -c "test -x '${_dir}/$sub_dir'" >/dev/null 2>&1 && _execable="exec"
+      #  if ! [[ "${_readable}" = "" && "${_writable}" = "" && "${_execable}" = "" ]]; then
+
+      #    x=0
+      #    _dir2="${_dir}/$sub_dir"
+      #    wc_=${#_dir2}
+      #    while [ "${wc_}" -gt "${x}" ] ; do
+      #      if [[ "$OS" = "Linux" ]]; then
+      #        sub_chr=${_dir2:${x}:${step_n}}
+      #      else
+      #        sub_chr=$(echo ${_dir2} | cut -c${x}-${step_n})
+      #      fi
+      #      if [ "${x}" -eq 0 ] ; then
+      #        printf "%-10s %-7s %-5s %-10s %-29s %-s \n" ${id} "${_readable}" "${_writable}" "${_execable}" "${sub_chr}" "${check_box}" >>${ACCESS_REPORT}
+      #      else
+      #        printf "%-35s %-s \n" " " "${sub_chr}" >>${ACCESS_REPORT}
+      #      fi
+      #      x=${x}+${step_n}
+      #    done
+
+      #  fi
+      #done
     done
-
-    echo ""
   done
-}
 
-create_base_permission() {
-  # 依照 DIRECTORY_YOU_WANT_TO_CHECK_PERMISSION 所指定的目錄
-  # 排除檔案 exclude_file 中列舉的檔案名稱
-  # 產生權限列表至 $BASE_PERMISSION
-  #
-  # 檔案格式為 檔名, 權限, uid, gid
-  # 範例
-  # /usr/bin/oldfind -rwxr-xr-x 0 0
-  # /usr/bin/catchsegv -rwxr-xr-x 0 0
-  # /usr/bin/xargs -rwxr-xr-x 0 0  
-  echo "Please wait..."
-  rm $BASE_PERMISSION >/dev/null 2>&1
-  for dir in $DIRECTORY_YOU_WANT_TO_CHECK_PERMISSION; do
-    echo "Parsing $dir permission now..."
+  if [ $ONE_OF_DIRECTORY_HAS_RWX_PERMISSION -eq 0 ];then
     echo ""
-    if [[ $OS = "Linux" ]]; then
-
-      for i in $(find $dir | grep -v -f exclude_file); do
-        stat -c '%n %A %g %u' $i >>$BASE_PERMISSION
-      done
-
-    else
-
-      for i in $(find $dir | grep -v -f exclude_file); do
-        echo $i" \c" >>$BASE_PERMISSION
-        istat $i | tr '\n' ' ' | awk '{print $8, $10, $12}' >>$BASE_PERMISSION
-      done
-
-    fi
-
-  done
-}
-
-create_permission_today() {
-  # 依照 DIRECTORY_YOU_WANT_TO_CHECK_PERMISSION 所指定的目錄
-  # 排除檔案 exclude_file 中列舉的檔案名稱  
-  # 產生權限列表至臨時檔案 $BASE_PERMISSION
-  #
-  # 檔案格式為 檔名, 權限, uid, gid
-  # 範例
-  # /usr/bin/oldfind -rwxr-xr-x 0 0
-  # /usr/bin/catchsegv -rwxr-xr-x 0 0
-  # /usr/bin/xargs -rwxr-xr-x 0 0
-
-  echo "Please wait..."
-  _permission_today="$RANDOM"_temp
-  for dir in $DIRECTORY_YOU_WANT_TO_CHECK_PERMISSION; do
-    echo "Parsing $dir permission now..."
-    echo ""
-    if [[ $OS = "Linux" ]]; then
-
-      for i in $(find $dir | grep -v -f exclude_file); do
-        stat -c '%n %A %g %u' $i >>$_permission_today
-      done
-
-    else
-
-      for i in $(find $dir | grep -v -f exclude_file); do
-        echo $i" \c" >>$_permission_today
-        istat $i | tr '\n' ' ' | awk '{print $8, $10, $12}' >>$_permission_today
-      done
-
-    fi
-
-  done
-}
-
-diff_permission_today_with_BASE() {
-  diff $BASE_PERMISSION $_permission_today >$PERMISSION_REPORT
-  if [[ $? -eq 0 ]]; then
-    echo ''
-    echo "Permission Audit passed."
-    echo "Permission Audit passed." >>$PERMISSION_REPORT
-
   else
-    echo ''
-    echo "Permission Audit failed. check $PERMISSION_REPORT for detail."
-    echo "Permission Audit failed." >>$PERMISSION_REPORT
-  fi
-  rm $_permission_today
-}
-
-check_permission() {
-  create_permission_today
-  diff_permission_today_with_BASE
-}
-
-create_base_md5() {
-  # 依照 DIRECTORY_YOU_WANT_TO_CHECK_MD5 所指定的目錄
-  # 排除檔案 exclude_file 中列舉的檔案名稱  
-  # 產生 md5 列表至 $BASE_MD5
-  #
-  # 檔案格式為 hash字串 full檔案路徑
-  # 範例：
-  # 85bc0fd26b358ea8edc0d4cab5e92044  /usr/bin/oldfind
-  # 795ad904fe7001acae1a149c4cd1ff3d  /usr/bin/catchsegv
-  # 2098c131c6f1f63777e9678b4be4e752  /usr/bin/xargs
-
-  echo "Please wait..."
-  rm $BASE_MD5 >/dev/null 2>&1
-  for dir in $DIRECTORY_YOU_WANT_TO_CHECK_MD5; do
-    echo "Parsing $dir hash now..."
-    echo ""
-    if [[ $OS = "Linux" ]]; then
-
-      for i in $(find $dir -type f | grep -v -f exclude_file); do
-        md5sum $i >>$BASE_MD5
-      done
-
-    else
-
-      for i in $(find $dir -type f | grep -v -f exclude_file); do
-        csum -h MD5 $i >>$BASE_MD5
-      done
-
-    fi
-
-  done
-}
-
-create_md5_today() {
-  # 依照 DIRECTORY_YOU_WANT_TO_CHECK_MD5 所指定的目錄
-  # 排除檔案 exclude_file 中列舉的檔案名稱  
-  # 產生 md5 列表至 $_md5_today
-  #
-  # 檔案格式為 hash字串 full檔案路徑
-  # 範例：
-  # 85bc0fd26b358ea8edc0d4cab5e92044  /usr/bin/oldfind
-  # 795ad904fe7001acae1a149c4cd1ff3d  /usr/bin/catchsegv
-  # 2098c131c6f1f63777e9678b4be4e752  /usr/bin/xargs
-
-  _md5_today="$RANDOM"_temp
-
-  for dir in $DIRECTORY_YOU_WANT_TO_CHECK_MD5; do
-    echo "Parsing $dir hash now..."
-    echo ""
-    if [[ $OS = "Linux" ]]; then
-
-      for i in $(find $dir -type f | grep -v -f exclude_file); do
-        md5sum $i >>$_md5_today
-      done
-
-    else
-
-      for i in $(find $dir -type f | grep -v -f exclude_file); do
-        csum -h MD5 $i >>$_md5_today
-      done
-
-    fi
-  done
-
-}
-
-diff_md5_today_with_BASE() {
-  diff $BASE_MD5 $_md5_today >$MD5_REPORT
-
-  if [[ $? -eq 0 ]]; then
-    echo ""
-    echo "MD5 Audit passed."
-    # echo "MD5 Audit passed." >>$MD5_REPORT
-  else
-    echo "檢查結果："
-    echo "============================================================="
-    cat $MD5_REPORT | grep ">\|<" | awk '{print $NF}' | sort | uniq | xargs -I {} echo "這些檔案發生變動： {}"
-    echo "============================================================="
-    echo "MD5 Audit failed."
-    echo "check $MD5_REPORT for detail."
+    echo "�����A���i���ݱb���ҵL�v���s���H�U���| $_DIR"  >>${ACCESS_REPORT} 
   fi
 
-  rm $_md5_today
+  if [ "${option_1}" != "${direct_option}" ] ; then
+    cp_report
+  fi
+
+
 }
 
-check_md5() {
-  create_md5_today
-  diff_md5_today_with_BASE
+
+list_last_ACCESS_REPORT() {
+  if [ -f "${ACCESS_REPORT}" ]; then
+    cat ${ACCESS_REPORT}
+    return
+  else
+    echo "�S�����Ѫ����i�C"
+    return
+  fi
+
+  reports=$(ls -tr ACCESS_*)
+
+  if [ -z "${reports}" ]; then
+    echo "�����͹L���i�A�а���ﶵ 1�C"
+    return
+  fi
+
+  last_report=$(ls -tr ACCESS_* | tail -1)
+  cat ${last_report}
+
 }
+
+
+skip_check_y() {
+  echo "                                 �ϥΪ̹ﭫ�n�t�Φ��A�����n�~���ɮ׻P�{���v���M�d" >>${ACCESS_REPORT}
+  echo '' >>${ACCESS_REPORT}
+  echo HOSTNAME: $(hostname) "    " TIME: $(date +%Y/%m/%d) $(date +%H:%M:%S) >>${ACCESS_REPORT}
+  echo '' >>${ACCESS_REPORT}
+  echo "�����A���L���n�~���ɮ׸��|" >>${ACCESS_REPORT}
+  echo '' >>${ACCESS_REPORT}
+}
+
+
+cp_report() {
+  \cp -f ${ACCESS_REPORT} ${dc01_REPORT}
+  chown dc01 ${dc01_REPORT}
+  chmod 770 ${dc01_REPORT}
+}
+
 
 main() {
   # The entry for sub functions.
@@ -339,12 +283,9 @@ main() {
     show_main_menu
     read choice
     clear
-    case $choice in
-    1) check_permission ;;
-    2) check_md5 ;;
-    3) create_base_permission ;;
-    4) create_base_md5 ;;
-    5) list_dirs_permissions_by_user ;;
+    case ${choice} in
+    1) list_dirs_permissions_by_user ;;
+    2) list_last_ACCESS_REPORT ;;
     [Qq])
       echo ''
       echo 'Thanks !! bye bye ^-^ !!!'
@@ -353,7 +294,6 @@ main() {
       logout
       ;;
     *)
-      clear
       clear
       echo ''
       echo ' !!!  ERROR CHOICE , PRESS ENTER TO CONTINUE ... !!!'
@@ -365,4 +305,13 @@ main() {
   done
 }
 
-main
+if [ "${option_1}" != "${direct_option}" ] ; then
+  main
+else
+  list_dirs_permissions_by_user
+fi
+
+rm -f ${0%/*}/*.temp
+
+exit 0
+
